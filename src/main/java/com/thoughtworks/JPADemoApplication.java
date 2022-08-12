d30e79d4a91fe64052efd7412e0dfa9dd3144fd5
Jiyang Zhang <JiyangZhang@ritchiebros.com>
2022-08-12 23:00:19 +0800

    init jpaDemo by docker, flyway and mysql

---
+++ src/main/java/com/thoughtworks/JPADemoApplication.java
@@ -0,0 +1,11 @@
+package com.thoughtworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPADemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JPADemoApplication.class, args);
+    }
+}
